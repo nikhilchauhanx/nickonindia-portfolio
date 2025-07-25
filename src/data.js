43ad824e0196db359262fe98583bbb2a16b61ba4
nikhilchauhanx <nikhilchauhan186@gmail.com>
2025-07-26 02:26:26 +0530

    feat: Add X social media icon and link

--- src/data.js
+++ src/data.js
@@ -1,16 +1,10 @@
 import { FaGithub, FaLinkedin, FaInstagram, FaYoutube, FaEnvelope, FaPhone } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
-// A custom component for the X/Twitter icon
-  const XIcon = () => (
-    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor">
-        <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
-    </svg>
-  );
-  
 export const socialLinks = [
   { name: "GitHub", href: "https://github.com/nikhilchauhanx", icon: FaGithub },
   { name: "LinkedIn", href: "https://linkedin.com/in/nickonindia", icon: FaLinkedin },
-  { name: "X", url: "https://x.com/nickonindia", icon: <XIcon /> },
+  { name: "X", href: "https://x.com/nickonindia", icon: FaXTwitter },
   { name: "Instagram", href: "https://instagram.com/nickonindia", icon: FaInstagram },
   { name: "YouTube", href: "https://www.youtube.com/@nickonindia", icon: FaYoutube },
   { name: "Email", href: "mailto:nikhilchauhan0618@gmail.com", icon: FaEnvelope },
